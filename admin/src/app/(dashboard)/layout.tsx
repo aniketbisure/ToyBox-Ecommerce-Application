85a3937e1243b51b177c1b45aac6f64345278b03
aniketbisure <bisureaniket03@gmail.com>
2026-07-15 15:20:39 +0530

    admin bug fix

--- admin/src/app/(dashboard)/layout.tsx
+++ admin/src/app/(dashboard)/layout.tsx
@@ -18,6 +18,7 @@ import {
   X,
 } from "lucide-react";
 import Logo from "../../components/Logo";
+import api from "../../services/apiService";
 
 export default function DashboardLayout({
   children,
